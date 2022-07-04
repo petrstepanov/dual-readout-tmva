30a8908e0f14b35f40c8ed3f95ece0fd9a25a930
Petr Stepanov <stepanovps@gmail.com>
2022-07-03 21:54:19 -0400

    Update StringUtils.cpp

--- src/StringUtils.cpp
+++ src/StringUtils.cpp
@@ -25,11 +25,11 @@ TString StringUtils::stripExtension(const char *str) {
 // TODO: move to some IO utils?
 void StringUtils::writeProgress(const char* s, Int_t nTimes){
     static Int_t counter = 0;
-    Int_t progress = ++counter/nTimes;
+    Int_t progressPercent = ++counter*100/nTimes;
 
     // Flush cout
     // https://stackoverflow.com/questions/3057977/rewinding-stdcout-to-go-back-to-the-beginning-of-a-line
-    std::cout << "\r" << s << ": " << progress*100 << "% (" << counter << "/" << nTimes << ") " << std::flush;
+    std::cout << "\r" << s << ": " << progressPercent << "% (" << counter << "/" << nTimes << ") " << std::flush;
     if (counter == nTimes){
         counter = 1;
         writeRewindEnd();
